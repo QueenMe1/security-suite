804f6270767ef8e5c1cf749b04082915386c6755
Susan O <110050655+QueenMe1@users.noreply.github.com>
2025-07-02 20:55:24 -0400

    testing password strength checker

--- password_checker/password_checker.py
+++ password_checker/password_checker.py
@@ -9,4 +9,29 @@ def password_strength_checker(password):
         score += 1;
     if re.search(r'\d', password):
         score += 1;
-    if 
+    if re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        score +=1;
+    if len(password) >= 8:
+        score += 1;
+
+    with open("Top_common_passwords.txt") as file:
+        if password in file.readline():
+            return "Weak (Common Password, Change it!!)";
+
+    if score <= 2:
+        return "Weak";
+    elif score == 3 or score ==4 :
+        return "Medium";
+    else:
+        return "Strong";
+
+
+def main ():
+
+    passwords = input("Enter your password: ")
+    print("Password Strength: " + password_strength_checker(passwords))
+
+
+if __name__ == '__main__':
+    main()
+    
